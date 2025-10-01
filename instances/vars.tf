bc7a0c7d3bc28d8cd37c866fa27b6782168bf732
Oluwole Ayodele <ayodeleoluwole112@gmail.com>
2025-10-01 00:00:00 -0500

    init: VPC, subnets, NAT gateways, VPC peering, and remote state backend

---
+++ instances/vars.tf
@@ -0,0 +1,18 @@
+variable "AWS_REGION" {
+  default = "us-east-2"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "test_instances_kp.pub"
+}
+
+
+variable "instance_type" {
+  default="t3.micro"
+}
+
+
+variable "ami" {
+  default="ami-0b671272c81662a99"
+}
+
